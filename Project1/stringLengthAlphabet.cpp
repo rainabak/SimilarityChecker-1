d62e5d3b14e6ed8cef522698639340e2cd57a5ac
raina <raina.bak@gmail.com>
2025-03-28 12:45:01 +0900

    retrun 부분 수정

--- Project1/stringLengthAlphabet.cpp
+++ Project1/stringLengthAlphabet.cpp
@@ -9,22 +9,21 @@ public:
 	}
 
 	int getScore(const std::vector<std::string>& wordVec) {
-		int score = 0;   
 		int len1 = length(wordVec[0]);
 		int len2 = length(wordVec[1]);
 		if (len1 == len2) {
-			score = 60;
+			return 60;
 		}
 		else {
 			int min = len1, max = len2;
 			if (len1 > len2)	min = len2, max = len1;
 
-			if (max >=  min * 2) {	score = 0;	}
+			if (max >=  min * 2) {	
+				return 0;	}
 			else {
-				int gap = max - min;
-				score = (int)((1.0 - (gap*1.0) / (min * 1.0)) * 60);				
+				return (int)((1.0 - ((max - min) * 1.0) / (min * 1.0)) * 60);
 			}
 		}
-		return score;
+		return 0;
 	}	
 };
